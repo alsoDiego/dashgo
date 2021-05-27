2e5d39e98b80f871be4853da733e1e8a033c7dff
Diego Soares <as.diego@live.com>
2021-05-27 15:32:31 -0300

    refactor: responsive Sidebar

--- src/components/Header/Index.tsx
+++ src/components/Header/Index.tsx
@@ -1,11 +1,15 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
 
 import { Logo } from "./Logo";
 import { SearchBox } from "./Search";
 import { Profile } from "./Profile";
 import { NotificationsNav } from "./NotificationsNav";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
 
 export function Header() {
+    const { onOpen } = useSidebarDrawer()
+
     const isWideVersion = useBreakpointValue ({
         base: false,
         md: true,
@@ -22,6 +26,18 @@ export function Header() {
           px="6"
           align="center"
         >
+            { !isWideVersion && (
+                <IconButton
+                    aria-label="Open navigation"
+                    icon={<Icon as={RiMenuLine} />}
+                    fontSize="24"
+                    variant="unstyled"
+                    onClick={onOpen}
+                    mr="2"
+                >
+                </IconButton>
+            )}
+
             <Logo />
 
             { isWideVersion && <SearchBox /> }
